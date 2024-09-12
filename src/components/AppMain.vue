c08f8915f9ab6598ef82aae2ffad40e36f25230d
Alessio Vietri <info@alessiovietri.it>
2024-09-12 11:55:10 +0200

    Completata v.2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,9 @@
 <script>
+import SingleCharacter from './SingleCharacter.vue';
 
 export default {
-  data() {
-    return {
-    }
+  components: {
+    SingleCharacter
   },
   props: {
     characters: Array
@@ -16,20 +16,7 @@ export default {
     <div class="container py-5">
       <div class="row">
         <div v-for="(character, i) in characters" :key="i" class="col-12 col-sm-6 col-md-3 mb-4">
-          <div>
-            <img :src="character.image" :alt="character.name" class="w-100 rounded-circle">
-          </div>
-
-          <h3>
-            {{ character.name }}
-          </h3>
-
-          <h6>
-            {{ character.status }}
-          </h6>
-          <h5>
-            {{ character.species }}
-          </h5>
+          <SingleCharacter :character="character" />
         </div>
       </div>
     </div>
---
+++ src/components/SingleCharacter.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    character: Object
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <img :src="character.image" :alt="character.name" class="w-100 rounded-circle">
+    </div>
+
+    <h3>
+      {{ character.name }}
+    </h3>
+
+    <h6>
+      {{ character.status }}
+    </h6>
+    <h5>
+      {{ character.species }}
+    </h5>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
